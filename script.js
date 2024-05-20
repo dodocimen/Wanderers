b02f72e7aa542cdd2415e64356582bceeacaa111
dodocimen <dogacimen35@gmail.com>
2024-05-20 14:11:04 -0400

    fixed popups+mobileoptimized

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 
-
 // FOR POPUP BUTTN
 function flipButton(element) {
     element.querySelector('.flipper').classList.toggle('flipped');
@@ -22,7 +21,7 @@ let rareWandererAttributes = [
       touchMessages: ["MOVE", "Don't slow me down!","I'm in a hurry!"],
       glowColor: [255, 0],
       dynamicSize: false,
-      spawnProbability: 0.01, // % chance to spawn
+      spawnProbability: 0.008, // % chance to spawn
       
     },
     {
@@ -42,7 +41,7 @@ let rareWandererAttributes = [
       touchMessages: ["Careful, I'm precious!", "Please be gentle!","Don't touch!"],
       glowColor: [255, 0],
       dynamicSize: false,
-      spawnProbability: 0.01, // % chance to spawn
+      spawnProbability: 0.008, // % chance to spawn
     },
     {
       name: "Frost",
@@ -57,7 +56,7 @@ let rareWandererAttributes = [
       touchMessages: ["You look brighter!", "Frostbite!","Sorry!"],
       glowColor: [0, 255, 255, 90],
       dynamicSize: false,
-      spawnProbability: 0.01, // % chance to spawn
+      spawnProbability: 0.009, // % chance to spawn
     },
     {
       name: "Phoenix",
@@ -77,7 +76,7 @@ let rareWandererAttributes = [
       touchMessages: ["Careful,hot!", "Don't get burned!","Sorry!"],
       glowColor: [255, 140, 0],
       dynamicSize: true,
-      spawnProbability: 0.01, // % chance to spawn
+      spawnProbability: 0.009, // % chance to spawn
     },
     {
       name: "Destroyer",
@@ -92,7 +91,7 @@ let rareWandererAttributes = [
       touchMessages: ["Aand gone", "You asked for it!", "BYE","whoops"],
       glowColor: [255, 0, 0, 0],
       dynamicSize: false,
-      spawnProbability: 0.01, // % chance to spawn
+      spawnProbability: 0.008, // % chance to spawn
     },
   ];
   
@@ -151,6 +150,7 @@ let rareWandererAttributes = [
     prevWindowWidth = windowWidth;
     prevWindowHeight = windowHeight;
     const infoWindow = document.getElementById("infoWindow");
+    
   
     if (windowWidth > 600) {
       infoWindow.addEventListener("mousedown", startDrag);
@@ -204,6 +204,8 @@ let rareWandererAttributes = [
       ? 30
       : 255; // Dark grey for dark mode or white for light mode
   }
+
+  
   
   function windowResized() {
     let widthChange = windowWidth - prevWindowWidth;
